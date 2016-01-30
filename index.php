af5f0feb82c5dd55af63ed48ac9c8d7e1fc186aa
Brittany Mazza <bmazza555@gmail.com>
2016-01-30 10:31:28 -0800

    John's changes

--- index.php
+++ index.php
@@ -1,23 +1,29 @@
 <?php
-// last update by BM 29-Jan @ 1937
+// updated by JL 30-Jan @ 0137
 require '../db_connection.php'; // Open database connection.
 
-function getBooks() {
+function getBooks($sort) {
     global $dbConn;
     $sql = "SELECT books.id, 
               books.title, 
-              books.isbn, 
-              books.release_date, 
               books.synopsis, 
               books.price, 
-              authors.name AS author, 
-              publishers.name AS publisher
+              authors.name AS author 
             FROM books 
             LEFT JOIN authors 
-            ON books.author_id = authors.id 
-            LEFT JOIN publishers 
-            ON books.publisher_id = publishers.id
-            ORDER BY books.id;";
+            ON books.author_id = authors.id ";
+	if (isset($_GET['sort']) && ($_GET['sort'] == 1)) {
+		$sql .= "ORDER BY books.title;";
+	}
+	elseif (isset($_GET['sort']) && ($_GET['sort'] == 2)) {
+		$sql .= "ORDER BY authors.name;";
+	}
+	elseif (isset($_GET['sort']) && ($_GET['sort'] == 3)) {
+			$sql .= "ORDER BY books.price;";
+	}
+	else {
+		$sql .= " ORDER BY books.id;";
+	}
     $stmt = $dbConn -> prepare($sql);
     $stmt -> execute();
     return $stmt->fetchAll();
@@ -30,33 +36,41 @@ function getBooks() {
   <title>Book Collection</title>
   <meta name="author" content="Team 6 - Bitsoft">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" href="../media/assign4/favicon.ico">
+  <!-- -->
+  <link rel="shortcut icon" href="../media/assign4-favicon.ico">
+  <!--<link rel="shortcut icon" href="favicon.ico">-->
   <style>
   /* Color Palette:
      #334139 -> Lighter Green    #1E2D24 -> Dark Green
      #E6E8E6 -> Off White        #FFE0B5 -> Light Tan
      #433F6B -> Indigo           */
   body {
-    background-image: url("../media/assign4/background-library.jpg");
+    background-image: url("../media/background-bookcase.jpg");
+    <!--background-image: url("background-bookcase.jpg");-->
   }
   h1 {
     color: #1E2D24;
     text-align: center;
+	text-shadow: -2px 2px #888888;
   }
   table {
     margin: auto;
     background-color: #E6E8E6;
   }
   tr:hover {
-    background-color: #433F6B;
-    color: white;
+    background-color: #FFE0B5;
   }
   th {
-    background-color: #FFE0B5;
+    background-color: #433F6B;
+	color: white;
+	text-shadow: -1px 1px #888888;
   }
   td {
     border-bottom: 1px solid #ddd;
     padding: 5px;
+  }
+  a {
+	  color: black;
   }
   .content {
     width: 1000px;
@@ -68,33 +82,55 @@ function getBooks() {
     width: 100%;
     height: 200%;
   }
+  .black_overlay {
+    display: none;
+    position: absolute;
+    top: 0%;
+    left: 0%;
+    width: 100%;
+    height: 100%;
+    background-color: black;
+    z-index:1001;
+    -moz-opacity: 0.8;
+    opacity:.80;
+    filter: alpha(opacity=80);
+  }
+  .white_content {
+    display: none;
+    position: absolute;
+    top: 25%;
+    left: 25%;
+    width: 50%;
+    height: 50%;
+    padding: 5px;
+    border: 5px solid #433F6B;
+    background-color: #E6E8E6;
+    z-index:1002;
+    overflow: auto;
+  }
   </style>
 </head>
 
 <body>
   <div class="content">
-    <h1>Book Collection</h1>
-    <table border=0 width=1000>
-      <tr>
-        <th width=100>Title</th> <!--Add clickable sort-by buttons -->
-        <th width=100>Author</th>
+	<h1>BITsoft Book Collection</h1>
+  <table border=0 width=1000>
+	<tr>
+        <th onClick="javascript:location.href='?sort=0'">ID<img src="../media/down.png"></th>
+		<th width=200 onClick="javascript:location.href='?sort=1'">Title<img src="../media/down.png"></th>
+        <th width=200 onClick="javascript:location.href='?sort=2'">Author<img src="../media/down.png"></th>
         <th>Synopsis</th>
-        <!--<th width=20>Release Date</th>
-        <th width=20>ISBN</th>
-        <th width=100>Publisher</th>-->
-        <th width=15>Price</th>
+        <th width=50 onClick="javascript:location.href='?sort=3'">Price<img src="../media/down.png"></th>
         <th> </th>
       </tr>
 <?php
   $bookList = getBooks();
   foreach ($bookList as $book) {
-    echo "\t\t\t<tr>\n";
+    echo "\t\t\t<tr onclick=\"document.getElementById('light').style.display='block';document.getElementById('fade').style.display='block'\">\n";
+    echo "\t\t\t\t<td><i>" . $book['id'] . "</i></td>\n";
     echo "\t\t\t\t<td><i>" . $book['title'] . "</i></td>\n";
     echo "\t\t\t\t<td>" . $book['author'] . "</td>\n";
-    echo "\t\t\t\t<td>" . substr($book['synopsis'], 0, 140) . "...</td>\n"; // Add an onClick pop-up that shows full synopsis & details
-    //echo "\t\t\t\t<td>" . $book['release_date'] . "</td>\n";
-    //echo "\t\t\t\t<td>" . $book['isbn'] . "</td>\n"; // Add these to details page (onClick)
-    //echo "\t\t\t\t<td>" . $book['publisher'] . "</td>\n";
+    echo "\t\t\t\t<td>" . substr($book['synopsis'], 0, 125) . "...</td>\n"; // Add an onClick pop-up that shows full synopsis & details
     echo "\t\t\t\t<td>" . $book['price'] . "</td>\n";
         echo "\t\t\t\t<td><a href=\".?edit=" . $book['id'] . "\">Edit</a></td>\n";
     echo "\t\t\t</tr>\n";
@@ -112,5 +148,17 @@ function getBooks() {
         <tr><td>Price:</td><td><input type=text name=price size=15 maxlength=10></td></tr>
       </table>
   </div>
+  <div id="light" class="white_content">
+	<table border=0>
+		<th colspan=2 align=right onclick = "document.getElementById('light').style.display='none';document.getElementById('fade').style.display='none'">Close</th>
+		<tr><td>Title</td><td rowspan=5>picOfCover</td></tr>
+		<tr><td>ISBN: isbn</td></tr>
+		<tr><td>by: author</td></tr>
+		<tr><td>Date: release_date</td></tr>
+		<tr><td>Pub: publisher</td></tr>
+		<tr><td colspan=2>Synopsis: synopsis goes here asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf </td></tr>
+	</table>
+	</div>
+  <div id="fade" class="black_overlay"></div>
 </body>
 </html>
